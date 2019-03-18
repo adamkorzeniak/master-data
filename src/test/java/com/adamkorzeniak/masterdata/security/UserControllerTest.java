a30ebf4a338e33fad73321e38fd15fa808637638
Adam Korzeniak <korzeniak.adam@gmail.com>
2019-03-18 22:52:48 +0100

    update

--- src/test/java/com/adamkorzeniak/masterdata/security/UserControllerTest.java
+++ src/test/java/com/adamkorzeniak/masterdata/security/UserControllerTest.java
@@ -1,6 +1,7 @@
 package com.adamkorzeniak.masterdata.security;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.mockito.Mockito.doReturn;
 import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
@@ -20,6 +21,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+import org.hamcrest.core.IsNull;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Matchers;
@@ -57,220 +59,26 @@ public class UserControllerTest {
 	private MockMvc mockMvc;
 
 	@Test
-	public void GetUserById_CorrectIdProvided_ReturnsUser() throws Exception {
-		Long id = 15L;
-		User mocked = new User();
-		mocked.setName("User");
-		mocked.setId(15L);
-		Optional<User> optional = Optional.of(mocked);
-
-		doReturn(optional).when(userService).findUserById(id);
-
-		mockMvc.perform(get(basePath + '/' + id)).andExpect(status().isOk())
-				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-				.andExpect(jsonPath("$.name", is("User")))
-				.andExpect(jsonPath("$.id", is(15)));
-	}
-
-	@Test
 	public void AddUser_NoIssues_ReturnsAddedUser() throws Exception {
 		UserDTO postUser = new UserDTO();
-		postUser.setName("Comedy");
+		postUser.setUsername("adam");
+		postUser.setPassword("password");
 		User mockUser = new User();
-		mockUser.setName("Comedy");
+		mockUser.setUsername("adam");
+		mockUser.setPassword("password");
 		
 	    String requestJson = convertToJson(postUser);
 
-		doReturn(mockUser).when(userService).addUser(Matchers.any());
+		doReturn(mockUser).when(userService).register(Matchers.any());
 
-		mockMvc.perform(post(basePath).contentType(MediaType.APPLICATION_JSON)
+		mockMvc.perform(post(basePath + "/register").contentType(MediaType.APPLICATION_JSON)
 				.content(requestJson))
 				.andExpect(status().isCreated())
 				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-				.andExpect(jsonPath("$.name", is("Comedy")));
+				.andExpect(jsonPath("$.username", is("adam")))
+				.andExpect(jsonPath("$.password", nullValue()));
 	}
 	
-	@Test
-	public void AddUser_MissingRequiredField_ReturnsBadRequestError() throws Exception {
-		UserDTO postUser = new UserDTO();
-		postUser.setId(100L);
-		
-	    String requestJson = convertToJson(postUser);
-	    String errorMessage = "Invalid 'name' field value: null. Field 'name' must not be blank.";
-	    
-		mockMvc.perform(post(basePath)
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(requestJson))
-				.andExpect(status().isBadRequest())
-				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-				.andExpect(jsonPath("$.code", is("REQ400")))
-				.andExpect(jsonPath("$.title", is("Bad Request")))
-				.andExpect(jsonPath("$.message", is(errorMessage)));
-	}
-	
-	@Test
-	public void UpdateUser_NoIssues_ReturnsUpdatedUser() throws Exception {
-		Long id = 11L;
-		UserDTO user = new UserDTO();
-		user.setId(id);
-		user.setName("Comedy");
-		User mockUser = new User();
-		mockUser.setId(id);
-		mockUser.setName("Comedy");
-		
-	    String requestJson = convertToJson(user);
-
-		doReturn(true).when(userService).isUserExist(id);
-		doReturn(mockUser).when(userService).updateUser(Matchers.anyLong(), Matchers.any());
-
-		mockMvc.perform(put(basePath + "/" + id)
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(requestJson))
-				.andExpect(status().isOk())
-				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-				.andExpect(jsonPath("$.id", is(11)))
-				.andExpect(jsonPath("$.name", is("Comedy")));
-	}
-	
-	@Test
-	public void UpdateUser_WrongIdProvided_ReturnsNotFoundError() throws Exception {
-		Long id = 11L;
-		UserDTO user = new UserDTO();
-		user.setId(id);
-		user.setName("Comedy");
-	    String requestJson = convertToJson(user);
-
-		doReturn(false).when(userService).isUserExist(id);
-	    
-		mockMvc.perform(put(basePath + "/" + id)
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(requestJson))
-				.andDo(print())
-				.andExpect(status().isNotFound())
-				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-				.andExpect(jsonPath("$.code", is("REQ404")))
-				.andExpect(jsonPath("$.title", is("Not Found")))
-				.andExpect(jsonPath("$.message", is("User not found: id=" + id)));
-	}
-
-	@Test
-	public void DeleteUser_CorrectIdProvided_DeletedUser() throws Exception {
-		Long id = 15L;
-
-		doReturn(true).when(userService).isUserExist(id);
-
-		mockMvc.perform(delete(basePath + "/" + id)).andExpect(status().isNoContent());
-	}
-
-	@Test
-	public void DeleteUser_WrongIdProvided_ReturnsNotFoundError() throws Exception {
-		Long id = 15L;
-
-		doReturn(false).when(userService).isUserExist(id);
-
-		mockMvc.perform(delete(basePath + "/" + id)).andExpect(status().isNotFound())
-				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-				.andExpect(jsonPath("$.code", is("REQ404")))
-				.andExpect(jsonPath("$.title", is("Not Found")))
-				.andExpect(jsonPath("$.message", is("User not found: id=" + id)));
-	}
-	
-	@Test
-	public void MergeUsers_NoIssues_ReturnsResultUser() throws Exception {
-		Long oldId = 11L;
-		Long newId = 22L;
-		
-		UserPatchDTO patchDTO = new UserPatchDTO();
-		patchDTO.setOp("merge");
-		patchDTO.setTargetUserId(newId);
-		
-		User targetUser = new User();
-		targetUser.setId(newId);
-		targetUser.setName("Comedy");
-		
-	    String requestJson = convertToJson(patchDTO);
-
-		doReturn(true).when(userService).isUserExist(oldId);
-		doReturn(true).when(userService).isUserExist(newId);
-		doReturn(targetUser).when(userService).mergeUsers(Matchers.anyLong(), Matchers.any());
-
-		mockMvc.perform(patch(basePath + "/" + oldId)
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(requestJson))
-				.andExpect(status().isOk())
-				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-				.andExpect(jsonPath("$.id", is(22)))
-				.andExpect(jsonPath("$.name", is("Comedy")));
-	}
-	
-	@Test
-	public void MergeUsers_SourceUserNotExists_ReturnsErrorResult() throws Exception {
-		Long oldId = 11L;
-		Long newId = 22L;
-		
-		UserPatchDTO patchDTO = new UserPatchDTO();
-		patchDTO.setOp("merge");
-		patchDTO.setTargetUserId(newId);
-		
-	    String requestJson = convertToJson(patchDTO);
-
-		doReturn(false).when(userService).isUserExist(oldId);
-		doReturn(true).when(userService).isUserExist(newId);
-
-		mockMvc.perform(patch(basePath + "/" + oldId)
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(requestJson))
-				.andExpect(status().isNotFound())
-				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-				.andExpect(jsonPath("$.code", is("REQ404")))
-				.andExpect(jsonPath("$.title", is("Not Found")))
-				.andExpect(jsonPath("$.message", is("User not found: id=11")));
-	}
-	
-	@Test
-	public void MergeUsers_TargetUserNotExists_ReturnsErrorResult() throws Exception {
-		Long oldId = 11L;
-		Long newId = 22L;
-		
-		UserPatchDTO patchDTO = new UserPatchDTO();
-		patchDTO.setOp("merge");
-		patchDTO.setTargetUserId(newId);
-		
-	    String requestJson = convertToJson(patchDTO);
-
-		doReturn(true).when(userService).isUserExist(oldId);
-		doReturn(false).when(userService).isUserExist(newId);
-
-		mockMvc.perform(patch(basePath + "/" + oldId)
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(requestJson))
-				.andExpect(status().isNotFound())
-				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-				.andExpect(jsonPath("$.code", is("REQ404")))
-				.andExpect(jsonPath("$.title", is("Not Found")))
-				.andExpect(jsonPath("$.message", is("User not found: id=22")));
-	}
-	
-	@Test
-	public void PatchUser_OperationNotSupported_ReturnsErrorResult() throws Exception {
-		Long newId = 22L;
-		
-		UserPatchDTO patchDTO = new UserPatchDTO();
-		patchDTO.setOp("rename");
-		patchDTO.setTargetUserId(newId);
-		
-	    String requestJson = convertToJson(patchDTO);
-
-		mockMvc.perform(patch(basePath + "/" + 11)
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(requestJson))
-				.andExpect(status().isBadRequest())
-				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-				.andExpect(jsonPath("$.code", is("REQ400")))
-				.andExpect(jsonPath("$.title", is("Bad Request")))
-				.andExpect(jsonPath("$.message", is("Operation 'rename' is not supported for Patch method on User resource.")));
-	}
-
 	private String convertToJson(UserDTO postUser) throws JsonProcessingException {
 		ObjectMapper mapper = new ObjectMapper();
 	    mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
