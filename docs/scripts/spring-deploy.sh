ad96bb9954e9f68f84ed049aec5e4f0b4431a620
Adam Korzeniak <korzeniak.adam@gmail.com>
2019-04-08 06:10:19 +0200

    Update

---
+++ docs/scripts/spring-deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd /home/adam/Development/repos/java/master-data
+echo Moved to project directory
+echo Run Build
+./mvnw clean package -DskipTests
+echo copy application to server
+scp target/master-data*.jar pi@192.168.0.103:/home/pi/server/app/master-data.jar
+
+echo raspberry pi
+ssh pi@192.168.0.103
+
+echo copy to location
+sudo cp /home/pi/server/app/master-data.jar /var/my-apps/master-data/
